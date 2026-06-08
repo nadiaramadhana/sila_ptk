2e34dd138a475d6b7266406adb2bc5498bcbc9cf
Aditya Prasetyo <aditzkun0987@gmail.com>
2026-06-08 19:04:48 +0700

    Add CRUD methods to DataPTKController

--- app/Http/Controllers/DataPTKController.php
+++ app/Http/Controllers/DataPTKController.php
@@ -39,4 +39,47 @@ class DataPTKController extends Controller
 
         return redirect()->route('data-ptk')->with('success', 'Data Berhasil Disimpan');
     }
+    
+    public function show($id)
+    {
+        $ptk = DataPTK::findOrFail($id);
+
+        return view('dashboard.data-ptk.show', compact('ptk'));
+    }
+
+    public function edit($id)
+    {
+        $ptk = DataPTK::findOrFail($id);
+
+        $kategori = KategoriPTK::all();
+        $jabatan = JabatanPTK::all();
+        $pangkat = PangkatPTK::all();
+
+        return view('dashboard.data-ptk.edit', compact('ptk', 'kategori', 'jabatan', 'pangkat'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $ptk = DataPTK::findOrFail($id);
+        $validate = $request->validate([
+            'kategori_id' => 'required|exists:kategori_ptk,id',
+            'nama_ptk' => 'required|string',
+            'tmt_pengangkatan' => 'date',
+            'jabatan_id' => 'required|exists:jabatan_ptk,id',
+            'pangkat_golongan_id' => 'required|exists:golongan_ptk,id',
+        ]);
+
+        $ptk->update($validate);
+
+        return redirect()->route('data-ptk')->with('success', 'Data PTK Berhasil di Perbaharui');
+    }
+
+    public function destroy($id)
+    {
+        $ptk = DataPTK::findOrFail($id);
+
+        $ptk->delete();
+
+        return redirect()->route('data-ptk')->with('success', 'Data PTK Berhasil di Hapus');
+    }
 }
